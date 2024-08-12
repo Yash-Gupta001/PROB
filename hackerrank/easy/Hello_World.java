e6cdfae5359ae0c5bf871c92cc67a92961454f56
Yash-Gupta001 <gyash4963@gmail.com>
2024-08-12 23:50:59 +0530

    a leetcode easy string question solved

---
+++ hackerrank/easy/Hello_World.java
@@ -0,0 +1,10 @@
+package hackerrank.easy;
+
+public class Hello_World {
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+// very first java prog for printing
