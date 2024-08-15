a91d337c2cccd17321ceb0940e13188cd75720e9
Yash-Gupta001 <gyash4963@gmail.com>
2024-08-15 14:50:48 +0530

    .

---
+++ hackerrank/easy/Taking_input.java
@@ -0,0 +1,21 @@
+package hackerrank.easy;
+import java.util.*;
+
+/**
+ * Taking_input
+ */
+public class Taking_input {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        Double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+        scan.close();
+    }
+}
