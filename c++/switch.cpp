9871c42447c0e2c5d324ea454bea702b6377ac15
Yash-Gupta001 <gyash4963@gmail.com>
2024-08-21 22:58:17 +0530

    all code corrected

--- c++/switch.cpp
+++ c++/switch.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
 using namespace std;
 
+
 int main() {
     char op;
-    float r, num2, num1, l;
+    float  num2, num1;
 
-    cout << "Enter an 1 for area of square"<<endl;
-    cout << "Enter an 2 for area of circle"<<endl;
-    cout << "Enter an 3 for area of rectangle"<<endl;
-    cin >> op;
+    cout << "Enter value"<<endl;
+    cin >> num1,num2;
 
     switch (op) {
-        case '1':
+        case '+':
         cout <<"enter height";
-        cin >> l;
-            cout <<"Area of square is "<< 4*l << endl;
+            cout <<"Addition "<< num1+num2 << endl;
             break;
 
-        case '2':
-        cout <<"enter radius";
-        cin >> r;
-            cout <<"Area of circle is " << 2*3.14*r << endl;
+        case '*':
+        cout <<"multiplication";
+            cout <<"multiplication " << num1*num2 << endl;
             break;
 
-        case '3':
-        cout <<"enter height and idth";
-        cin >> num1 >> num2;
-            cout << "Area of rectangle " << num1 * num2 << endl;
+        case '-':
+        cout <<"subraction";
+            cout << "subraction" << num1 - num2 << endl;
             break;
 
+        case '/':
+        cout <<"div";
+            cout << "div" << num/num2 << endl;
+            break;
         default:
             cout << "not correct." << endl;
             break;
