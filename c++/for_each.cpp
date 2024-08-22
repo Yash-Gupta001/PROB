047850d4f29e378e0a70dc7f9266281071f50434
Yash-Gupta001 <gyash4963@gmail.com>
2024-08-22 23:33:26 +0530

    for each studied

---
+++ c++/for_each.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[] ={20,15,2,5,84,654,546,6,5461,614,465,85,654,5} ;
+    for(int i : arr){
+        cout <<"value-:"<< i << endl;
+    }
+}
