2561fa31d3e124977aefeb8d4fc99d01a9d9bdd2
Yash-Gupta001 <gyash4963@gmail.com>
2024-08-25 15:27:09 +0530

    palindrome nad reverse

--- c++/char_datatype.cpp
+++ c++/char_datatype.cpp
@@ -11,5 +11,16 @@ int main(){
     cout << str << endl;   // Output the string
     cout << c << endl;     // Output the single character
 
+    // Different ways to initialize a string
+    string str1 = "Hello, World!";
+    string str2("Hello, C++!");
+    string str3;
+    str3 = "String example";
+
+    // Outputting the strings
+    cout << str1 << endl;
+    cout << str2 << endl;
+    cout << str3 << endl;
+    
     return 0;
 }
