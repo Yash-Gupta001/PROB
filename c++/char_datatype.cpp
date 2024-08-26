b568dc1256267b8d3c4795f61f5f49de50f277ce
Yash-Gupta001 <gyash4963@gmail.com>
2024-08-26 23:12:53 +0530

    string datatype

--- c++/char_datatype.cpp
+++ c++/char_datatype.cpp
@@ -7,7 +7,7 @@ int main(){
 
     char str[100];     // Define a character array with a fixed size
     cin >> str;        // Read a string into the character array
-
+    //cin.get(str);
     cout << str << endl;   // Output the string
     cout << c << endl;     // Output the single character
 
@@ -16,11 +16,23 @@ int main(){
     string str2("Hello, C++!");
     string str3;
     str3 = "String example";
+    //cin.get(str3);
 
     // Outputting the strings
     cout << str1 << endl;
     cout << str2 << endl;
     cout << str3 << endl;
+
+    // for printing total line 
+    cout << "enter val";
+    char val[100];
+    cin >> val;
+    cin.get(val,100);
+    cout << val;
     
     return 0;
 }
+/*
+even upper
+odd lo
+*/
