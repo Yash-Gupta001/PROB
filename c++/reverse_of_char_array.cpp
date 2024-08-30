677ddc02d57f4096f0e5a2072aff8ee2c7ba3c67
Yash-Gupta001 <gyash4963@gmail.com>
2024-08-30 19:19:03 +0530

    revese program solved of array

---
+++ c++/reverse_of_char_array.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Rev {
+    public:
+    char arr[30];
+
+    void getData() {
+        cout << "Enter array: " << endl;
+        cin.get(arr, 30); 
+    }
+
+    void logic() {
+        int len = arr.strlen(arr);
+        for (int i = len - 1; i >= 0; i--) {
+            cout << arr[i];
+        }
+        cout << endl; 
+    }
+};
+
+int main() {
+    Rev rev;       
+    rev.getData(); 
+    rev.logic();   
+    return 0;
+}
