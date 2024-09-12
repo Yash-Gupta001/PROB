1937de086b294550ccae4c7e84b546be5bdd55ca
Yash-Gupta001 <gyash4963@gmail.com>
2024-09-12 20:20:29 +0530

    Bitwise operator in java

--- basic_java/Boolean_logical_operator.java
+++ basic_java/Boolean_logical_operator.java
@@ -13,6 +13,11 @@ public class Boolean_logical_operator {
 }
 
 /*
+ * if we use single & (AND) in logical operation and first condition is false it will still check the second condition knowing that first is false.
+ * and same for | (OR) 
+ */
+
+/*
  * output-:
  * 3
  * 6
